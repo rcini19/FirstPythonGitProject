941c5104e6d63688be4cf4caff740472e1a8eb74
rcini19 <gamallobenzleo@gmail.com>
2025-09-22 02:08:05 +0800

    add views

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        if title:  # prevent empty tasks
+            Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'add_task.html')
